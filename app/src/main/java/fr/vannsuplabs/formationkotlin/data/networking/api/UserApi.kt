38566ca37bb182dfcde498dbe8527e7d4641a368
TOUR-GAMERYANN\yannk <yann.kerleguer@orange.fr>
2022-02-01 20:09:37 +0100

    Mise a jour version lib + import inutiliser

--- app/src/main/java/fr/vannsuplabs/formationkotlin/data/networking/api/UserApi.kt
+++ app/src/main/java/fr/vannsuplabs/formationkotlin/data/networking/api/UserApi.kt
@@ -4,7 +4,6 @@ import fr.vannsuplabs.formationkotlin.data.model.PaginatedResult
 import fr.vannsuplabs.formationkotlin.data.model.User
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.Path
 import retrofit2.http.Query
 
